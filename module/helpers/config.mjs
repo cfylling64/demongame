cd1787fce8e48340a13769ca33b2c341cc9e5f13
cayce <cfylling@ucmerced.edu>
2023-07-26 17:19:55 -0700

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const DEMONGAME = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ DEMONGAME.abilities = {
+  "str": "DEMONGAME.AbilityStr",
+  "dex": "DEMONGAME.AbilityDex",
+  "con": "DEMONGAME.AbilityCon",
+  "int": "DEMONGAME.AbilityInt",
+  "wis": "DEMONGAME.AbilityWis",
+  "cha": "DEMONGAME.AbilityCha"
+};
+
+DEMONGAME.abilityAbbreviations = {
+  "str": "DEMONGAME.AbilityStrAbbr",
+  "dex": "DEMONGAME.AbilityDexAbbr",
+  "con": "DEMONGAME.AbilityConAbbr",
+  "int": "DEMONGAME.AbilityIntAbbr",
+  "wis": "DEMONGAME.AbilityWisAbbr",
+  "cha": "DEMONGAME.AbilityChaAbbr"
+};
